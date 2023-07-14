505e2423bd6e1d025cf19c3abc12a24c48cd65ff
Calco Dev <calcopoddev@gmail.com>
2023-07-14 17:16:45 +0300

    User help page done.

--- src/react/components/account/elements/help.component.tsx
+++ src/react/components/account/elements/help.component.tsx
@@ -1,5 +1,52 @@
+import React from 'react';
+import {AccountElementContainer, AccountSettingsHeader} from './utils.tsx';
+import TextInput from '../../inputs/input/text-input.component.tsx';
+import styled from 'styled-components';
+
+const Link = styled.a`
+    color: #18a5ec;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const Subtext = styled.p`
+    font-size: 0.75rem;
+    color: #a0a0a0;
+    margin-top: 0.25rem;
+`;
+
 const Help = () => {
-    return <div>HELP & SUPPORT AAA</div>;
+    return (
+        <AccountElementContainer>
+            <AccountSettingsHeader>Help</AccountSettingsHeader>
+
+            <AccountElementContainer>
+                <TextInput
+                    label={'Support Email'}
+                    value={'support@inkwell.systems'}
+                    onChange={_ => {}}
+                />
+
+                <div>
+                    <TextInput
+                        label={'Documentation'}
+                        value={'https://github.com/Inkwell-Systems/InkwellAPI'}
+                        onChange={_ => {}}
+                    />
+                    <Subtext>
+                        Or click{' '}
+                        <Link href="https://github.com/Inkwell-Systems/InkwellAPI">
+                            here
+                        </Link>
+                        .
+                    </Subtext>
+                </div>
+            </AccountElementContainer>
+        </AccountElementContainer>
+    );
 };
 
 export default Help;
