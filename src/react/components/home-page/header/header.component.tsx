402578e6f518cee5ac631aacd0dda8becfb97f38
Calco Dev <calcopoddev@gmail.com>
2023-07-14 17:22:56 +0300

    Fixed homepage button links.

--- src/react/components/home-page/header/header.component.tsx
+++ src/react/components/home-page/header/header.component.tsx
@@ -7,8 +7,15 @@ import {
     SubHeader,
 } from './header.styles.tsx';
 import {Button} from '../../inputs/button/button.component.tsx';
+import {useNavigate} from 'react-router-dom';
 
 const HomepageHeader = () => {
+    const nav = useNavigate();
+
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noreferrer');
+    };
+
     return (
         <Container>
             <LandingImage src={HomeLandingLibrary} alt={'Landing image.'} />
@@ -33,12 +40,18 @@ const HomepageHeader = () => {
                 <Button
                     config={{style: 'primary', inverted: false}}
                     style={{marginRight: '2.5em'}}
+                    onClick={() => nav('/sign/in')}
                 >
                     Get Started
                 </Button>
                 <Button
                     config={{style: 'secondary', inverted: true}}
                     style={{marginRight: '2.5em'}}
+                    onClick={() =>
+                        openInNewTab(
+                            'https://github.com/Inkwell-Systems/Inkwell',
+                        )
+                    }
                 >
                     Contribute
                 </Button>
